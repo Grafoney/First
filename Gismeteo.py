30e47c748b862591b469c6356dbe3a8d1da1554a
Grafoney <vantx2s@gmail.com>
2016-05-14 01:05:58 +0300

    Add files via upload

---
+++ Gismeteo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def main():
+    print(get_html('https://www.gismeteo.ru/'))
+
+print(main())
