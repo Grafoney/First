c02c71544e257c442b80123936822b241571c661
Grafoney <vantx2s@gmail.com>
2016-05-14 15:16:55 +0300

    Update Gismeteo.py

--- Gismeteo.py
+++ Gismeteo.py
@@ -8,9 +8,14 @@ def get_html(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'}
     r = requests.get(url, headers=headers)
     return r.text
-
+def parse(html):
+    soup = BeautifulSoup(html)
+    h2 = soup.find("h2", class_ = "typeC")
+    print (h2.prettify())
+    dd = soup.find("dd",class_ = "value m_temp c")
+    print (dd.prettify())
 
 def main():
-    print(get_html(url))
+    parse(get_html(url))
 
 print(main())
